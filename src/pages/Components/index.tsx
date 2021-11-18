b4e7fcefc2cb0a9f41e25c43c794d86a63815235
Daniel Pescador <daniel.pescador@brewninja.net>
2021-11-18 12:42:43 -0600

    Added new component to Components

--- src/pages/Components/index.tsx
+++ src/pages/Components/index.tsx
@@ -12,41 +12,35 @@ export const ComponentsPage = () => {
           <h4>Properties</h4>
         </div>
         <div className='col-6'>
-          <Card name={mockData[0].Name} desc={mockData[0].Data} link={{ url: mockData[0].Link.Url, text: mockData[0].Link.Text }} />
+          <Card />
         </div >
         <div className='col-6'>
-          <CardClass name={mockData[1].Name} desc={mockData[1].Data} link={{ url: mockData[1].Link.Url, text: mockData[1].Link.Text }} />
+          <CardClass />
         </div >
       </div >
       <div className='row mb-5'>
         <div className='col-12'>
           <h4>Childrens</h4>
-          <ChildrenExample sidebar={<h1>Hello there!</h1>}>
-            <>
-              <Card name={mockData[0].Name} desc={mockData[0].Data} link={{ url: mockData[0].Link.Url, text: mockData[0].Link.Text }} />
-              <CardClass name={mockData[1].Name} desc={mockData[1].Data} link={{ url: mockData[1].Link.Url, text: mockData[1].Link.Text }} />
-            </>
-          </ChildrenExample>
         </div>
       </div>
       <div className='row mb-5'>
         <Link to='/components/solution'>Solution</Link>
       </div>
 
-    </MainContainer>
+    </MainContainer >
   )
 }
 
 // FUNCTIONAL COMPONENT
-const Card = ({ name, desc, link }: ICardProps): ReactElement => {
+const Card = (): ReactElement => {
   return (
     <div className='col-6'>
       <h5>Functional component</h5>
       <div className="card" >
         <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">{desc}</p>
-          <a href={link.url} className="btn btn-primary">{link.text}</a>
+          <h5 className="card-title">Card name</h5>
+          <p className="card-text">Card Description</p>
+          <a href=' ' className="btn btn-primary">Link</a>
         </div>
       </div>
     </div>
@@ -55,24 +49,47 @@ const Card = ({ name, desc, link }: ICardProps): ReactElement => {
 
 
 // CLASS COMPONENT
-class CardClass extends Component<ICardProps, any> {
-
+class CardClass extends Component<any, any> {
 
   render() {
-    const { name, desc, link } = this.props;
     return (
       <div className='col-6'>
         <h5>Class component</h5>
-        <div className="card">
+        <div className="card" >
           <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <p className="card-text">{desc}</p>
-            <a href={link.url} className="btn btn-primary">{link.text}</a>
+            <h5 className="card-title">Card name</h5>
+            <p className="card-text">Card Description</p>
+            <a href=' ' className="btn btn-primary">Link</a>
           </div>
         </div>
       </div>
     )
   }
+}
+
+interface ICardBody extends ICardProps {
+  themeColor: 'btn-primary' | 'btn-success'
+}
+
+
+interface ICardProps {
+  name: string;
+  desc: string;
+  link: {
+    url: string;
+    text: string
+  }
+}
+
+
+const CardBody = ({ name, desc, link, themeColor }: ICardBody) => {
+  return (
+    <div className="card-body bg-transparent">
+      <h5 className="card-title">{name}</h5>
+      <p className="card-text">{desc}</p>
+      <a href={link.url} className={`btn ${themeColor ? themeColor : 'btn-primary'}`} >{link.text}</a>
+    </div>
+  )
 }
 
 const ChildrenExample = ({ children, sidebar }: { children?: ReactElement, sidebar?: ReactElement }): ReactElement => {
@@ -88,15 +105,6 @@ const ChildrenExample = ({ children, sidebar }: { children?: ReactElement, sideb
       </div>
     </div>
   )
-}
-
-interface ICardProps {
-  name: string;
-  desc: string;
-  link: {
-    url: string;
-    text: string
-  }
 }
 
 const mockData = [
@@ -125,17 +133,3 @@ const mockData = [
     }
   },
 ]
-
-interface ICardBody extends ICardProps {
-  themeColor: 'btn-primary' | 'btn-success'
-}
-
-const CardBody = ({ name, desc, link, themeColor }: ICardBody) => {
-  return (
-    <div className="card-body bg-transparent">
-      <h5 className="card-title">{name}</h5>
-      <p className="card-text">{desc}</p>
-      <a href={link.url} className={`btn ${themeColor ? themeColor : 'btn-primary'}`} >{link.text}</a>
-    </div>
-  )
-}
